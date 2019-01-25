de63ccc085e9ce8339c67136d04d7ef8f8cd12e0
alex_ <alex_huedo@hotmail.com>
2019-01-25 11:08:04 +0100

    versión conectada con FireBase

--- app/src/main/java/com/example/alex_/proyectointegrador/MainActivity.java
+++ app/src/main/java/com/example/alex_/proyectointegrador/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.alex_.proyectointegrador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvTitulo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTitulo = (TextView) findViewById(R.id.tvTitulo);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        tvTitulo.startAnimation(animation);
     }
 }
