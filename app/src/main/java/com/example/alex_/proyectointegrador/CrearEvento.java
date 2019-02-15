ff0cbae68a4c34ac1259a060a8b3faa498a55dff
Dani <danisom1b@gmail.com>
2019-02-15 10:46:35 +0100

    crear eventos

---
+++ app/src/main/java/com/example/alex_/proyectointegrador/CrearEvento.java
@@ -0,0 +1,13 @@
+package com.example.alex_.proyectointegrador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_evento);
+    }
+}
